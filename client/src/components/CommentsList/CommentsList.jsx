d23530922326aa572bcb97e4628d085707b7f0f1
Thessa Kranendonk <thessakranendonk@Thessas-MacBook-Air.local>
2021-12-02 00:34:09 -0500

    styled CommentsList component

--- client/src/components/CommentsList/CommentsList.jsx
+++ client/src/components/CommentsList/CommentsList.jsx
@@ -5,17 +5,19 @@ function CommentsList(props) {
     <li key={props.id} className="comments-display">
 
       <div className="comments-display__wrapper">
-        <h3 className="comments-display__name">{props.name}</h3>
-        <p className="comments-display__date">
-          {new Date(props.timestamp).toLocaleDateString()}
-        </p>
+        <div className="comments-display__user-info">
+          <h3 className="comments-display__name">{props.name}</h3>
+          <p className="comments-display__date">
+            {new Date(props.timestamp).toLocaleDateString()}
+          </p>
+        </div>
         <p className="comments-display__comment">{props.comment}</p>
-        <button
-          className="comments-display__delete-button"
-        //   onClick={() => props.handleDeleteComment(props.commentId)}
-        >
+        {/* <button
+          className="comments-display__delete-button" */}
+        {/* onClick={() => props.handleDeleteComment(props.commentId)} */}
+        {/* >
           Delete
-        </button>
+        </button> */}
       </div>
     </li>
   );
