a89bd5c01fcdf82f219e0e9c1283f58817da53b0
Thessa Kranendonk <thessakranendonk@Thessas-MacBook-Air.local>
2021-12-01 23:35:09 -0500

    styled FoodSection

--- client/src/components/FoodSection/FoodSection.jsx
+++ client/src/components/FoodSection/FoodSection.jsx
@@ -5,8 +5,19 @@ const FoodSection = (props) => {
       <>
      
     <li className="food-section" key={props.id}>
-      <p className="food-section__dish">Dish {props.dish}</p>
-        <p className="food-section__description">Cuisine {props.cuisine}</p>
+    <div className="food-section__background"></div>
+    <div className="food-section__main">
+      <div className="food-section__name-wrapper">
+        <p className="food-section__dish">Dish</p>
+        <p className="food-section__description">Cuisine</p>
+      </div>
+      <div className="food-section__info-wrapper">
+        <p className="food-section__dish-entry">{props.dish}</p>
+        <p className="food-section__description-entry">{props.cuisine}</p>
+      </div>
+      </div>
+
+        
     </li>
 
     
