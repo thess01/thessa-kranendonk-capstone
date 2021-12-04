95956f7765613a3cd921e2f04c5974cefe24fdcd
Thessa Kranendonk <thessakranendonk@gmail.com>
2021-12-04 18:47:24 -0500

    styled foodsection for different screen sizes

--- client/src/pages/SingleBeerPage/SingleBeerPage.js
+++ client/src/pages/SingleBeerPage/SingleBeerPage.js
@@ -9,80 +9,73 @@ import "./SingleBeerPage.scss";
 class SingleBeerPage extends Component {
   state = {
     selectedBeer: null,
-    errorLoading: false
+    errorLoading: false,
   };
 
   componentDidMount() {
-      const id = this.props.match.params.id;
-    Promise.all([
-      axios.get(`/api/beers/${id}`),
-
-    ])
+    const id = this.props.match.params.id;
+    Promise.all([axios.get(`/api/beers/${id}`)])
       .then(([res1, res2]) => {
         this.setState({
           selectedBeer: res1.data,
-          errorLoading: false
+          errorLoading: false,
         });
       })
       .catch((error) => {
         console.log(error);
         this.setState({
-          errorLoading: true
-        })
+          errorLoading: true,
+        });
       });
   }
 
   getBeerById = (id) => {
-    axios.get(`/api/beers/${id}`)
-    .then((response) => {
-      this.setState({
-        selectedBeer: response.data,
+    axios
+      .get(`/api/beers/${id}`)
+      .then((response) => {
+        this.setState({
+          selectedBeer: response.data,
+        });
       })
-    })
-    .catch((error) => {
-      console.log(error)
-      this.setState({
-        errorLoading: true
-      })
-    })
-  }
-  
+      .catch((error) => {
+        console.log(error);
+        this.setState({
+          errorLoading: true,
+        });
+      });
+  };
+
   addComment = (e) => {
     e.preventDefault();
 
     let newComment = {
       comment: e.target.comment.value,
       userName: e.target.name.value,
-      beer_id: this.props.match.params.id
-
+      beer_id: this.props.match.params.id,
     };
 
     let beerId = this.props.match.params.id;
-    axios.post(`/api/beers/${beerId}`, newComment)
-    .then((response) => {
-
-      let beerId = this.props.match.params.id;
-      this.getBeerById(beerId);
-
-    })
-    .catch((error) => {
-      console.log("Can't post comment")
-    })
+    axios
+      .post(`/api/beers/${beerId}`, newComment)
+      .then((response) => {
+        let beerId = this.props.match.params.id;
+        this.getBeerById(beerId);
+      })
+      .catch((error) => {
+        console.log("Can't post comment");
+      });
     e.target.comment.value = "";
-  }
+  };
 
   handleDeleteComment = (commentId) => {
     let beer_id = this.props.match.params.id;
-    axios.delete(`/api/beers/${beer_id}/comments/${commentId}`)
-    .then(() => {
+    axios.delete(`/api/beers/${beer_id}/comments/${commentId}`).then(() => {
       this.getBeerById(beer_id);
-    })
-  }
-
+    });
+  };
 
   render() {
     const { selectedBeer } = this.state;
-
 
     if (this.state.selectedBeer === null) {
       return (
@@ -92,45 +85,28 @@ class SingleBeerPage extends Component {
       );
     }
 
-
-    console.log(this.state.selectedBeer.comments)
-
-    
+    console.log(this.state.selectedBeer.comments);
 
     return (
       <>
-        <BeerDetails 
-        beer={selectedBeer} />
+        <BeerDetails beer={selectedBeer} />
+        <FoodSection foods={this.state.selectedBeer.dishes} />
+        <CommentsForm addComment={this.addComment} />
 
-        {/* {selectedBeer.dishes.map((dish) => ( */}
-            <FoodSection foods={this.state.selectedBeer.dishes} />
-        {/* //     dish={dish.dish}
-        //     cuisine={dish.cuisine}
-        //     beerType={dish.beerType}
-        //     food_image={dish.food_image} />
-        // ))}  */}
-        
-
-        <CommentsForm addComment={this.addComment}/>
-        
         <div className="single-beer-page__container">
-          {this.state.selectedBeer.comments ?
-          (
-
-          <CommentsList comments={this.state.selectedBeer.comments} handleDeleteComment={this.handleDeleteComment}/>
-         
-        ) : (
+          {this.state.selectedBeer.comments ? (
+            <CommentsList
+              comments={this.state.selectedBeer.comments}
+              handleDeleteComment={this.handleDeleteComment}
+            />
+          ) : (
             <p>Leave a comment</p>
-        )}
-        {this.state.errorLoading && (
-          <p>No comments yet</p>
-        )}
+          )}
+          {this.state.errorLoading && <p>No comments yet</p>}
         </div>
-        
-    </>
+      </>
     );
   }
 }
-
 
 export default SingleBeerPage;
