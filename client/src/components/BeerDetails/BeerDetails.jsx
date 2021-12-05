a3fbaf6009bd169739eabc4318b2d7c3f04621a0
Thessa Kranendonk <thessakranendonk@gmail.com>
2021-12-05 16:27:35 -0500

    finished styling for tablet for all components

--- client/src/components/BeerDetails/BeerDetails.jsx
+++ client/src/components/BeerDetails/BeerDetails.jsx
@@ -6,33 +6,27 @@ const BeerDetails = ({beer}) => {
   return (
     <div className="beer-details">
       <div className="beer-details__background"></div>
-     <img className="beer-details__image" src={beer.image} alt={beer.beerName}/>
+      <img className="beer-details__image" src={beer.image} alt={beer.beerName}/>
       <div className="beer-details__brewery-wrap">
-        <h4 className="beer-details__beer-name">{beer.beerName}</h4>
+        <h4 className="beer-details__beer-name">{beer.beerName.toUpperCase()}</h4>
         <p className="beer-details__brewery-info">by {beer.breweryName}, {beer.cityState}, {beer.country}</p>
       </div>
-      <p className="beer-details__beer-type">{beer.beerType}</p>
+      <p className="beer-details__beer-type">{beer.beerType.toUpperCase()} - {beer.ABV}</p>
 
 
-      <div className="beer-details__desktop-wrapper">
+   
         <p className="beer-details__description">{beer.description}</p>
         <div className="beer-details__wrapper">
-            <div className="beer-details__name-wrapper">
-              <p className="beer-details__name">Category</p>
-              <p className="beer-details__name">Season</p>
-              <p className="beer-details__name">Flavor</p>
-              <p className="beer-details__name">ABV</p>
-              <p className="beer-details__name">Rating</p>
-            </div>
-            <div className="beer-details__info-wrapper">
-            <p className="beer-details__info">{beer.beerType}</p>
-            <p className="beer-details__info">{beer.season}</p>
-            <p className="beer-details__info">{beer.flavor}</p>
-            <p className="beer-details__info">{beer.ABV}</p>
-            <p className="beer-details__info">{beer.rating} stars</p>
-            </div>
+        
+              <p className="beer-details__name">Season:</p>
+              <p className="beer-details__info">{beer.season}</p>
+              <p className="beer-details__name">Profile:</p>
+              <p className="beer-details__info">{beer.flavor}</p>
+              <p className="beer-details__name">Expert rating:</p>
+              <p className="beer-details__info">{beer.rating} stars</p>
+          
         </div>
-      </div>
+      
   
     </div>
   );
