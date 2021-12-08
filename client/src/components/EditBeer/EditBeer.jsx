bbf771dfb9ef55cdbe2c48046ec0d779ad1c7a9c
Thessa Kranendonk <thessakranendonk@gmail.com>
2021-12-08 00:16:45 -0500

    front end form and modal

--- client/src/components/EditBeer/EditBeer.jsx
+++ client/src/components/EditBeer/EditBeer.jsx
@@ -1,129 +1,51 @@
-import { useEffect, useState } from "react";
-import {useHistory} from 'react-router-dom';
-import EditBeer from '../EditBeer/EditBeer';
+import './EditBeer.scss';
+import { Link, useHistory } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import axios from "axios";
 import Box from "@mui/material/Box";
 import React from "react";
 import Modal from "@mui/material/Modal";
-import { useForm} from 'react-hook-form'
-import "./BeerDetails.scss";
 
-const BeerDetails = ({beer, handleEditBeer}) => {
-const [breweryInfo, setBreweryInfo] = useState(null);
-const [open, setOpen] = React.useState(false);
-  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+
+export default function EditBeer({breweryInfo}) {
+  const [open, setOpen] = React.useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
   console.log(errors);
-
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const onSubmit = (data) => {
-    axios
-      .put(`/api/beers/${beer.id}`, data)
-      .then(() => {
-        alert("Upload Succesful!");
-        handleClose()
-
-    }, [reset])
-      .catch(err => {console.log(err)});
-
-  
-}
-
-useEffect(() => {
-    getBreweryData();
-}, []);
-
-
-  const getBreweryData = () => {
-    let token = sessionStorage.getItem('authToken')
-    if (!!token) {
-        axios.get('/api/upload', {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
-            .then(response => {
-                const breweryData = response.data;
-                setBreweryInfo(breweryData);
-            })
-            .catch((error) => {
-                console.log(error);
-              });
-    } 
-  }
   let history = useHistory();
 
-  const handleDelete = () => {
-    axios.delete(`/api/beers/${beer.id}`)
+  const onSubmit = (data) => {
+    axios
+      .put(`/api/beers/${breweryInfo.id}`, data)
       .then(() => {
-          history.push("/");
-          })
+        alert("Upload Succesful!");
+        handleClose();
+      }, [reset])
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  }
-
-
-  if (!breweryInfo || beer.brewery_id !== breweryInfo.brewery_id) {
   return (
-    <div className="beer-details">
-      <div className="beer-details__background"></div>
-      <img className="beer-details__image" src={beer.image} alt={beer.beerName}/>
-      <div className="beer-details__brewery-wrap">
-        <h4 className="beer-details__beer-name">{beer.beerName.toUpperCase()}</h4>
-        <p className="beer-details__brewery-info">by {beer.breweryName}, {beer.cityState}, {beer.country}</p>
-      </div>
-      <p className="beer-details__beer-type">{beer.beerType.toUpperCase()} - {beer.ABV}</p>
-
-
-   
-        <p className="beer-details__description">{beer.description}</p>
-        <div className="beer-details__wrapper">
-        
-              <p className="beer-details__name">Season:</p>
-              <p className="beer-details__info">{beer.season}</p>
-              <p className="beer-details__name">Profile:</p>
-              <p className="beer-details__info">{beer.flavor}</p>
-              <p className="beer-details__name">Expert rating:</p>
-              <p className="beer-details__info">{beer.rating} stars</p>
-        </div>
-    </div>
-  )
-  } else {
-    return (
-    <div className="beer-details">
-      <div className="beer-details__background"></div>
-      <img className="beer-details__image" src={beer.image} alt={beer.beerName}/>
-      <div className="beer-details__brewery-wrap">
-        <h4 className="beer-details__beer-name">{beer.beerName.toUpperCase()}</h4>
-        <p className="beer-details__brewery-info">by {beer.breweryName}, {beer.cityState}, {beer.country}</p>
-      </div>
-      <p className="beer-details__beer-type">{beer.beerType.toUpperCase()} - {beer.ABV}</p>
-
-
-   
-        <p className="beer-details__description">{beer.description}</p>
-        <div className="beer-details__wrapper">
-        
-              <p className="beer-details__name">Season:</p>
-              <p className="beer-details__info">{beer.season}</p>
-              <p className="beer-details__name">Profile:</p>
-              <p className="beer-details__info">{beer.flavor}</p>
-              <p className="beer-details__name">Expert rating:</p>
-              <p className="beer-details__info">{beer.rating} stars</p>
-        </div>
-        <button className="delModal__delete-button" onClick={handleOpen}>Edit</button>
-        <button className="delModal__delete-button" onClick={handleDelete}>Delete</button>
-    
-    <div className="edit-beer">
-    <Modal
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box className="edit-beer__modal-box">
-      <h2 className="edit-beer__modal-header">Enter Beer Details</h2>
+    <>
+      <section className="edit-beer">
+        <div className="edit-beer__container">
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box className="edit-beer__modal-box">
+              <h2 className="edit-beer__modal-header">Enter Beer Details</h2>
               <form
                 className="edit-beer__form"
                 onSubmit={handleSubmit(onSubmit)}
@@ -235,14 +157,10 @@ useEffect(() => {
                   <input className="edit-beer__submit" type="submit" />
                 </div>
               </form>
-
-      </Box>
-    </Modal>
-  </div>
-  </div>
-    )
-  }
-          }
-
-
-export default BeerDetails;
+            </Box>
+          </Modal>
+        </div>
+      </section>
+    </>
+  );
+}
